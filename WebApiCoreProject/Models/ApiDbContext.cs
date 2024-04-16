26ddff5c1f9a202dbb60484f1c389dd68a2ec2a6
safaehayani3@gmail.com <safaehayani3@gmail.com>
2024-04-16 13:44:32 +0200

    Ajoutez des fichiers projet.

---
+++ WebApiCoreProject/Models/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApiCoreProject.Models
+{
+    public class ApiDbContext : DbContext
+    {
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+
+        }
+       
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
---
+++ WebApiCoreProject/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiCoreProject.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName="varchar(200)")]
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; }
+        public int ContactNumber { get; set;}
+       
+
+
+    }
+}
